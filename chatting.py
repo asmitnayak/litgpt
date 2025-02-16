4caa8badd8d4401e6812b010cf49c9977b20ebd9
Asmit Nayak <asmit.nayak@gmail.com>
2025-02-16 02:36:00 -0500

    updating chatting.py

--- chatting.py
+++ chatting.py
@@ -96,7 +96,7 @@ class DPChat:
         print(f"Now chatting with {self.config.name}.\n")
         L.seed_everything(1234)
 
-    def process_prompt(self, prompt, temperature, max_new_tokens, top_k, top_p):
+    def process_prompt(self, prompt, temperature, max_new_tokens, top_k, top_p, stream=False):
         prompt = self.prompt_style.apply(prompt=prompt)
         encoded_prompt = self.tokenizer.encode(prompt, device=self.fabric.device)
 
@@ -118,9 +118,10 @@ class DPChat:
 
         tokens_generated = 0
         generated_content = ""
-        for tok in tqdm(token_generator):
+        for tok in token_generator:
             tokens_generated += 1
-            self.fabric.print(tok, end="", flush=True)  # TODO: Change this
+            if stream:
+                self.fabric.print(tok, end="", flush=True)
             generated_content += tok
 
         t = time.perf_counter() - t0
@@ -130,11 +131,13 @@ class DPChat:
 
         return generated_content
 
-    def interact(self, prompt: str, temperature: float, max_new_tokens: int, top_k: int = 50, top_p: float = 1.0):
+    def interact(self,
+                 prompt: str, temperature: float, max_new_tokens: int,
+                 top_k: int = 50, top_p: float = 1.0,
+                 stream=False):
         """
-
         Args:
-            prompt:
+            prompt: The prompt to start the conversation with.
             max_new_tokens: The number of generation steps to take.
             top_k: The number of top most probable tokens to consider in the sampling process.
             top_p: If specified, it represents the cumulative probability threshold to consider in the sampling process.
@@ -155,12 +158,13 @@ class DPChat:
                 samples.
         """
         return self.process_prompt(
-                    prompt,
-                    temperature,
-                    max_new_tokens,
-                    top_k,
-                    top_p
-                )
+                prompt,
+                temperature,
+                max_new_tokens,
+                top_k,
+                top_p,
+                stream
+            )
 
     @torch.inference_mode()
     def generate( self,
